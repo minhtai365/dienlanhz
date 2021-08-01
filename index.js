801fdcd4b8733e1f453bef11e4525120b73c9013
Minh Tài <tranminhtai365@gmail.com>
2021-08-02 00:26:38 +0700

    add public

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 var express = require('express');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
-
+var path = require('path');
 const bodyParser = require("body-parser");
 
 var usersRouter = require('./routes/users');
@@ -32,6 +32,7 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
 
 // app.use('/', indexRouter);
 app.use('/users', usersRouter);
